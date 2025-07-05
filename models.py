f1f05928f8cc02d8b1a73d49a8b0060ea71263e5
chris <ivi3001@yahoo.de>
2025-07-05 21:41:59 +0200

    backup

--- models.py
+++ models.py
@@ -10,47 +10,47 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        self.conv1 = nn.Conv2d(1, 32, 3)
+        self.conv1 = nn.Conv2d(1, 32, 5)
         self.bn1 = nn.BatchNorm2d(32)
         self.pool1 = nn.MaxPool2d(2, 2)
-        self.drop1 = nn.Dropout(p=0.1)
+        self.drop1 = nn.Dropout(p=0.0)
         
-        self.conv2 = nn.Conv2d(32, 64, 3)
+        self.conv2 = nn.Conv2d(32, 64, 5)
         self.bn2 = nn.BatchNorm2d(64)
         self.pool2 = nn.MaxPool2d(2, 2)
-        self.drop2 = nn.Dropout(p=0.1)
+        self.drop2 = nn.Dropout(p=0.0)
         
         self.conv3 = nn.Conv2d(64, 128, 3)
         self.bn3 = nn.BatchNorm2d(128)
         self.pool3 = nn.MaxPool2d(2, 2)
-        self.drop3 = nn.Dropout(p=0.1)
+        self.drop3 = nn.Dropout(p=0.0)
         
         self.conv4 = nn.Conv2d(128, 256, 3)
         self.bn4 = nn.BatchNorm2d(256)
         self.pool4 = nn.MaxPool2d(2, 2)
-        self.drop4 = nn.Dropout(p=0.1)
+        self.drop4 = nn.Dropout(p=0.0)
         
         self.conv5 = nn.Conv2d(256, 512, 3)
         self.bn5 = nn.BatchNorm2d(512)
         self.pool5 = nn.MaxPool2d(2, 2)
-        self.drop5 = nn.Dropout(p=0.1)
+        self.drop5 = nn.Dropout(p=0.0)
         
         self.flattened_size = self._get_flattened_size()
         print("flattened_size:", self.flattened_size)
 
-        self.fc1 =  nn.Linear(self.flattened_size, 512)
-        self.fc1_bn = nn.BatchNorm1d(512)
+        self.fc1 =  nn.Linear(self.flattened_size, 256)
+        self.fc1_bn = nn.BatchNorm1d(256)
         self.fc1_drop = nn.Dropout(p=0.4)
         
-        self.fc2 =  nn.Linear(512, 512)
-        self.fc2_bn = nn.BatchNorm1d(512)
+        self.fc2 =  nn.Linear(256, 256)
+        self.fc2_bn = nn.BatchNorm1d(256)
         self.fc2_drop = nn.Dropout(p=0.4)
         
-        self.fc3 =  nn.Linear(512, 512)
-        self.fc3_bn = nn.BatchNorm1d(512)
+        self.fc3 =  nn.Linear(256, 256)
+        self.fc3_bn = nn.BatchNorm1d(256)
         self.fc3_drop = nn.Dropout(p=0.2)
         
-        self.fc4 =  nn.Linear(512, 136)    
+        self.fc4 =  nn.Linear(256, 136)    
         
     def _get_flattened_size(self):
         with torch.no_grad():
@@ -74,7 +74,7 @@ class Net(nn.Module):
         
         x = self.fc1_drop(F.relu(self.fc1_bn(self.fc1(x))))
         x = self.fc2_drop(F.relu(self.fc2_bn(self.fc2(x))))
-        #x = self.fc3_drop(F.relu(self.fc3_bn(self.fc3(x))))     
+        x = self.fc3_drop(F.relu(self.fc3_bn(self.fc3(x))))     
         x = self.fc4(x)     
         
         return x
