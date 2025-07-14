14d3e99d0c1b10457c09d3b92da62ea5ce17059f
chris <ivi3001@yahoo.de>
2025-07-14 16:18:19 +0200

    finished project for submission

--- data_load.py
+++ data_load.py
@@ -60,11 +60,11 @@ class Normalize(object):
 
         # convert image to grayscale
         image_copy = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        
-        # scale color range from [0, 255] to [0, 1]
-        image_copy=  image_copy/255.0
-            
-        
+
+        # Determine whether image is already in [0,1] range
+        if image_copy.dtype == np.uint8 or image_copy.max() > 1.0:
+            image_copy = image_copy / 255.0
+                 
         # scale keypoints to be centered around 0 with a range of [-1, 1]
         # mean = 100, sqrt = 50, so, pts should be (pts - 100)/50
         key_pts_copy = (key_pts_copy - 100)/50.0
@@ -107,6 +107,67 @@ class Rescale(object):
 
         return {'image': img, 'keypoints': key_pts}
 
+class RandomRotate(object):
+    def __init__(self, max_angle_deg, scale=1.0, center=None):
+        self.angle = max_angle_deg
+        self.scale = scale
+        self.center = center
+        
+    def __call__(self, sample):
+        image, key_pts = sample['image'], sample['keypoints']
+        h, w = image.shape[:2]
+        
+        if self.center is None:
+            self.center = (w / 2, h / 2)
+    
+        self.angle = np.random.uniform(-self.angle, self.angle)
+        
+        # Get rotation matrix
+        rot_mat = cv2.getRotationMatrix2D(self.center, self.angle, self.scale)
+
+        # Rotate image
+        rotated_image = cv2.warpAffine(image, rot_mat, (w, h))
+
+        # Convert keypoints to homogeneous coordinates and apply rotation
+        ones = np.ones((key_pts.shape[0], 1))
+        key_pts_hom = np.hstack([key_pts, ones])  # Nx3
+        rotated_key_pts =key_pts_hom @ rot_mat.T  # Nx2
+
+        return {'image': rotated_image, 'keypoints': rotated_key_pts}
+
+
+class RandomHorizontalFlip(object):
+    def __init__(self, prob=0.5):
+        self.prob = prob
+        self.symmetric_pairs = [
+            (0, 16), (1, 15), (2, 14), (3, 13), (4, 12), (5, 11), (6, 10), (7, 9),
+            (17, 26), (18, 25), (19, 24), (20, 23), (21, 22),
+            (31, 35), (32, 34),
+            (36, 45), (37, 44), (38, 43), (39, 42), (40, 47), (41, 46),
+            (48, 54), (49, 53), (50, 52),
+            (61, 63), (60, 64),
+            (67, 65), (58, 56), (59, 55)
+        ]
+
+    def __call__(self, sample):
+        image, key_pts = sample['image'], sample['keypoints']
+
+        if np.random.rand() < self.prob:
+            h, w = image.shape[:2]
+
+            # Flip image horizontally
+            image = cv2.flip(image, 1)
+
+            # Flip keypoints: x -> w - x
+            key_pts = key_pts.copy()
+            key_pts[:, 0] = (w-1) - key_pts[:, 0]
+
+            # Swap symmetric keypoints
+            for i, j in self.symmetric_pairs:
+                key_pts[[i, j]] = key_pts[[j, i]]
+
+        return {'image': image, 'keypoints': key_pts}
+      
 
 class RandomCrop(object):
     """Crop randomly the image in a sample.
--- models.py
+++ models.py
@@ -10,47 +10,55 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 32, 7, padding=(3,3))
         self.bn1 = nn.BatchNorm2d(32)
         self.pool1 = nn.MaxPool2d(2, 2)
         self.drop1 = nn.Dropout(p=0.0)
         
-        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv2 = nn.Conv2d(32, 64, 5, padding=(2,2))
         self.bn2 = nn.BatchNorm2d(64)
         self.pool2 = nn.MaxPool2d(2, 2)
         self.drop2 = nn.Dropout(p=0.0)
         
-        self.conv3 = nn.Conv2d(64, 128, 3)
+        self.conv3 = nn.Conv2d(64, 128, 5, padding=(2,2))
         self.bn3 = nn.BatchNorm2d(128)
         self.pool3 = nn.MaxPool2d(2, 2)
         self.drop3 = nn.Dropout(p=0.0)
         
-        self.conv4 = nn.Conv2d(128, 256, 3)
+        self.conv4 = nn.Conv2d(128, 256, 3, padding=(1,1))
         self.bn4 = nn.BatchNorm2d(256)
         self.pool4 = nn.MaxPool2d(2, 2)
         self.drop4 = nn.Dropout(p=0.0)
         
-        self.conv5 = nn.Conv2d(256, 512, 3)
+        self.conv5 = nn.Conv2d(256, 512, 3, padding=(1,1))
         self.bn5 = nn.BatchNorm2d(512)
         self.pool5 = nn.MaxPool2d(2, 2)
         self.drop5 = nn.Dropout(p=0.0)
         
+        #self.conv6 = nn.Conv2d(512, 1024, 3, padding=(1,1))
+        #self.bn6 = nn.BatchNorm2d(1024)
+        #self.pool6 = nn.MaxPool2d(1, 1)
+        #self.drop6 = nn.Dropout(p=0.0)
+        
         self.flattened_size = self._get_flattened_size()
         print("flattened_size:", self.flattened_size)
 
-        self.fc1 =  nn.Linear(self.flattened_size, 256)
-        self.fc1_bn = nn.BatchNorm1d(256)
-        self.fc1_drop = nn.Dropout(p=0.4)
+        self.linear_count = 128 
+        self.linear_drop_p = 0.4
         
-        self.fc2 =  nn.Linear(256, 256)
-        self.fc2_bn = nn.BatchNorm1d(256)
-        self.fc2_drop = nn.Dropout(p=0.4)
+        self.fc1 =  nn.Linear(self.flattened_size, self.linear_count)
+        self.fc1_bn = nn.BatchNorm1d(self.linear_count)
+        self.fc1_drop = nn.Dropout(p=self.linear_drop_p)
         
-        self.fc3 =  nn.Linear(256, 256)
-        self.fc3_bn = nn.BatchNorm1d(256)
-        self.fc3_drop = nn.Dropout(p=0.2)
+        self.fc2 =  nn.Linear(self.linear_count, self.linear_count)
+        self.fc2_bn = nn.BatchNorm1d(self.linear_count)
+        self.fc2_drop = nn.Dropout(p=self.linear_drop_p)
         
-        self.fc4 =  nn.Linear(256, 136)    
+        self.fc3 =  nn.Linear(self.linear_count, self.linear_count)
+        self.fc3_bn = nn.BatchNorm1d(self.linear_count)
+        self.fc3_drop = nn.Dropout(p=self.linear_drop_p)
+        
+        self.fc4 =  nn.Linear(self.linear_count, 136)    
         
     def _get_flattened_size(self):
         with torch.no_grad():
@@ -60,6 +68,7 @@ class Net(nn.Module):
             x = self.drop3(self.pool3(F.relu(self.bn3(self.conv3(x)))))
             x = self.drop4(self.pool4(F.relu(self.bn4(self.conv4(x)))))
             x = self.drop5(self.pool5(F.relu(self.bn5(self.conv5(x)))))
+            #x = self.drop6(self.pool6(F.relu(self.bn6(self.conv6(x)))))
             return x.view(1, -1).size(1)
         
     def forward(self, x):
@@ -69,6 +78,7 @@ class Net(nn.Module):
         x = self.drop3(self.pool3(F.relu(self.bn3(self.conv3(x)))))
         x = self.drop4(self.pool4(F.relu(self.bn4(self.conv4(x)))))
         x = self.drop5(self.pool5(F.relu(self.bn5(self.conv5(x)))))
+        #x = self.drop6(self.pool6(F.relu(self.bn6(self.conv6(x)))))
         
         x = x.view(x.size(0), -1)
         
